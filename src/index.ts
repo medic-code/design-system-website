7ac297ed0efab2aa175407de4a1c9c738d8dda72
Aaron Smith <u40as7@gmail.com>
2023-03-04 14:14:36 +0000

    chore(fix): Add exports to components created for package

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,9 @@ export * from "./lib/typography";
 export { default as Anchor } from './components/Anchor';
 export { default as Button } from './components/Button';
 export { default as Typography } from './components/Typography';
+export { default as Grid } from './components/Grid';
+export { default as Flex } from './components/Flex';
+export { default as Box } from './components/Box';
+
+
 export * from './components/Icon';
