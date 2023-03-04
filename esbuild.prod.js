24914d8b2ce332193d0a79cafd8d95564aafe6e2
Aaron Smith <u40as7@gmail.com>
2023-03-04 19:25:27 +0000

    chore(fix): Add cjs and esm for building package

--- esbuild.prod.js
+++ esbuild.prod.js
@@ -1,18 +1,36 @@
-import esbuild from 'esbuild';
-import { globPlugin } from 'esbuild-plugin-glob';
+const esbuild = require('esbuild');
+const { globPlugin } = require('esbuild-plugin-glob');
 
-esbuild.build({
-    color: true,
-    entryPoints: ["./src/index.ts", "src/components/**/index.ts"],
+const sharedConfig = {
+    loader: {
+        '.tsx': 'tsx',
+        '.ts':'ts',
+    },
+    outbase: './src',
     bundle: true,
     minify: true,
-    splitting: true,
-    outdir: "dist",
-    sourcemap: true,
-    format: "esm",
-    logLevel: "debug",
     jsxFactory: 'createElement',
     jsxFragment: 'Fragment',
+    target: ['esnext'],
+    logLevel: 'debug',
+}
+
+esbuild.build({
+    ...sharedConfig,
+    entryPoints: ['./src/index.ts'],
+    outdir: 'dist/cjs',
+    format: 'cjs',
+
+}).catch(() => process.exit(1));
+
+esbuild.build({
+    ...sharedConfig,
+    color: true,
+    entryPoints: ["./src/index.ts", "src/components/**/index.ts"],
+    splitting: true,
+    outdir: "dist/esm",
+    sourcemap: true,
+    format: "esm",
     plugins: [globPlugin()]
 })
 .catch(() => process.exit(1));
